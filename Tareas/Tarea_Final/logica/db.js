387e10cfc394b457b03109734a9417afb5a5fbe6
AldairH <aldairhj@outlook.com>
2025-05-25 22:14:19 -0600

    Update Final

--- Tareas/Tarea_Final/logica/db.js
+++ Tareas/Tarea_Final/logica/db.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
 
 db.connect(err => {
     if (err) console.error('Error al conectar DB:', err);
-    else console.log('Conectado a la DB');
+    else console.log('Si jala XD');
 });
 
 module.exports = db;
